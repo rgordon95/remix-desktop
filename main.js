1d0e7d0852df14b2a7e08cf1e65372a6e4ef9e8c
Richard Gordon <richgordon95@gmail.com>
2019-04-24 19:14:24 -0400

    add icon

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 768,
+    icon: __dirname + '/favicon.ico',
     webPreferences: {
       nodeIntegration: true
     }
@@ -20,6 +21,9 @@ function createWindow () {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+
+
+  
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -34,6 +38,8 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
